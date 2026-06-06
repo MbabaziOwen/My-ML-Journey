c00d79af958605011274cf538d9d5cb73ab71e1c
MbabaziOwen <163897840+MbabaziOwen@users.noreply.github.com>
2026-06-06 14:59:13 +0300

    if statement
    if statement

---
+++ Learning_phases/Phase_1/Python/if_statements.py
@@ -0,0 +1,8 @@
+response = input(" would yu=ou like food?  (Y/N)")
+
+if response == "Y":
+    print("Have some food")
+elif response == "N":
+    print("No food for you")
+else:
+    print("enter a valid input") 
---
+++ Learning_phases/Phase_1/Python/test_python.py
@@ -0,0 +1,7 @@
+name = input("Please enter your name: ")
+age = int(input("what is you age: "))
+New_age = age + 1
+
+print(f"hello, {name}")
+print(f"your age is , {age}")
+print(f"you will be , {New_age} next year  ")
