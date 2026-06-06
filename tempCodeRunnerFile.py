de50468e1345d190e91695f40fc7712e383aa1be
MbabaziOwen <163897840+MbabaziOwen@users.noreply.github.com>
2026-06-06 15:17:00 +0300

    calculator
    calculator using if statements

---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,22 @@
+#python calculator
+
+operator = input("Enter an operator(+ - * /): ")
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
+if operator =="+":
+    result  = num1 + num2
+elif operator =="/":
+    result = num1/num2
+elif operator =="-":
+    result = num1-num2
+elif operator == "*":
+    result = num1*num2
+else:
+    print("enter the right symbol")
+
+print(result)
+
+
+
+
