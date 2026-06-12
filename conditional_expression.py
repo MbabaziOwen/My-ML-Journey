fe3e525d46e7d31d4ae650f64e310ac916bfe48b
MbabaziOwen <163897840+MbabaziOwen@users.noreply.github.com>
2026-06-12 10:50:26 +0300

    new

---
+++ conditional_expression.py
@@ -0,0 +1,7 @@
+num = 5
+
+#print("positive" if num > 0 else "Negative")
+
+result = "EVEN" if num%2 == 0 else "ODD"
+print(result)
+
---
+++ whileloop.py
@@ -0,0 +1,6 @@
+name = input("enter you name ")
+
+while name =="":
+    print("you did not enter your name ")
+    name = input("Enter your name: ")
+print(f"Hello {name}")
